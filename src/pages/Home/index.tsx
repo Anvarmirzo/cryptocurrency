b47d19ffb7920b311db040a8c98813f6f157edb6
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-05-29 17:49:51 +0500

    completed homepage (#2)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Navbar} from "../../components";
-import {Layout} from "antd";
+import {Col, Row, Statistic, Typography} from 'antd';
 
-interface HomePageProps {
-
-};
-export const HomePage = (props: HomePageProps) => {
+export const HomePage = () => {
     return (
-        <div>Home</div>
+        <>
+            <Typography.Title level={2} className="heading">Global Crypto Stats</Typography.Title>
+            <Row>
+                <Col span={12}><Statistic title='Total Cryptocurrencies' value={5}/></Col>
+                <Col span={12}><Statistic title='Total Exchanges' value={5}/></Col>
+                <Col span={12}><Statistic title='Total Market Cap' value={5}/></Col>
+                <Col span={12}><Statistic title='Total 24h Volume' value={5}/></Col>
+                <Col span={12}><Statistic title='Total Markets' value={5}/></Col>
+            </Row>
+        </>
     );
 };
